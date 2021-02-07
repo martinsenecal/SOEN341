f134b5c229ba353f0fe52b196b2a30f59e2f7e8a
Martin Senecal <martinsenecal2000@gmail.com>
2021-02-07 17:06:38 -0500

    #32 - Setup MaterialUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 // Import Components Here
 import './static/style/App.css';
+import Button from '@material-ui/core/Button';
 
 const App = () => {
   return (
     <div>
       <h1>Hello!</h1>
+      <Button variant="contained" color="primary">
+        Hello World. I am a Button made with Material UI
+      </Button>
     </div>
   );
 };
