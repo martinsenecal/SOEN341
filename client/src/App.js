a81fa8e87ebf1108c7f22f94234f87eaa6fca8ea
MilitsaB <m_bogdev@live.concordia.ca>
2021-02-11 01:22:02 -0500

    #15 - Modified main component of the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-// Import Components Here
+import React from 'react';
 import './static/style/App.css';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-const App = () => {
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import Header from './components/layout/Header';
+import Landing from './components/layout/Landing';
+
+const useStyles = makeStyles({});
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Hello!</h1>
-      <Button variant="contained" color="primary">
-        Hello World! I am a Button made with Material UI
-      </Button>
+    <div className={classes.container}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/signIn" component={SignIn} />
+          <Route exact path="/signUp" component={SignUp} />
+        </Switch>
+      </Router>
     </div>
   );
-};
-
-export default App;
+}
