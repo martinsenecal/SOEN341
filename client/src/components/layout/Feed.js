8e5e2207ff6379b9460d8180ec30be23425b8ec4
martinsenecal <martinsenecal2000@gmail.com>
2021-02-14 15:18:19 -0500

    #15 - Add some test for the feed authentication

--- client/src/components/layout/Feed.js
+++ client/src/components/layout/Feed.js
@@ -6,9 +6,17 @@ const Feed = () => {
   const [auth, setAuth] = useContext(AuthContext);
 
   return (
-    <div>
-      <h1>This is the feed! Wohoooo!</h1>
-    </div>
+    <>
+      {auth.loading || auth.user === null ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {' '}
+          <h1>This is the feed! Wohoooo!</h1>
+          <h2>Your username is: {auth.user.username}</h2>
+        </>
+      )}
+    </>
   );
 };
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div>
       <h1>
-        This is the landing Page! Just for testing.. We won't use this page.
+        This is the landing Page! Just for testing.. We will not use this page.
       </h1>
       <h2>Here is the Auth (Global State)</h2>
       <p> isAuthenticated: {auth.isAuthenticated ? 'True' : 'False'}</p>
