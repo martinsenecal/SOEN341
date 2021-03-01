bdfa4ff66eaf1b0647ae752e8ce3c35310863e9d
amanda6mard <61531228+amanda6mard@users.noreply.github.com>
2021-03-01 14:05:51 -0500

    #24 added the follow/unfollow button to user tags

--- client/src/components/building-blocks/FollowButton.js
+++ client/src/components/building-blocks/FollowButton.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 
-const FollowButton = ({ user, visiting }) => {
-  //default state depends on whether profile's user is in logged-in user's "following" list (variable to be replaced)
+const FollowButton = ({username, extraClass, visiting}) => {
+  //default state depends on whether profile's user is in logged-in user's "following" list (variable 'visiting' to be replaced)
   const [authUser, setAuthUser] = useState(visiting);
 
   const toggleFollowed = () => {
     setAuthUser((visiting.isFollowingUser = !visiting.isFollowingUser));
+    //add/remove 'username' from auth.user followee list.
   };
 
   return (
     <button
-      className={`follow-button btn ${
+      className={`follow-button btn  ${extraClass} ${
         visiting.isFollowingUser ? 'btn-outline-primary' : 'btn-primary'
       }`}
       onClick={toggleFollowed}
--- client/src/components/building-blocks/UserTag.js
+++ client/src/components/building-blocks/UserTag.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import FollowButton from './FollowButton';
+
+//hard-coded example of the user who is viewing the tag. (eventually auth.user)
+const visitingUser = {
+  username: 'example02',
+  isFollowingUser: false,
+};
 
 const UserTag = ({username, profilePicture}) => {
   return (
-    <Link to={'/profile/' + username} target="_parent">
-      <div className="row align-items-center">
-        <div className="col-xs-auto profile-picture-col">
-          <div className="card-profile-picture-container ml-3">
-            <img
-              src={profilePicture}
-              alt={username}
-              className="rounded-circle card-profile-picture"
-            />
+    <div className="row align-items-center">
+      <div className="col-auto">
+        <Link to={'/profile/' + username} target="_parent">
+          <div className="row align-items-center">
+            <div className="col-xs-auto profile-picture-col">
+              <div className="card-profile-picture-container ml-3">
+                <img
+                  src={profilePicture}
+                  alt={username}
+                  className="rounded-circle card-profile-picture"
+                />
+              </div>
+            </div>
+            <div className="col">
+              <div className="text-muted d-inline-block">{username}</div>
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="text-muted d-inline-block">{username}</div>
-        </div>
+        </Link>
       </div>
-    </Link>
+      <div className="col">
+        {
+          //visiting user to be switched to auth.user in condition, removed from follow button
+          username === visitingUser.username ? (
+            ''
+          ) : (
+            <FollowButton
+              extraClass="btn-sm py-0"
+              username={username}
+              visiting={visitingUser}
+            />
+          )
+        }
+      </div>
+    </div>
   );
 };
 
