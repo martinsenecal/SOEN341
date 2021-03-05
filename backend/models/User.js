1d94596f43c9f0bb4e0598b43b424645f6aa908c
KD <kim199@yahoo.com>
2021-03-05 15:32:55 -0500

    #24 Added Following Array to the user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
@@ -29,6 +30,27 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  following: [
+    {
+      //user following
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      //user to be followed
+      following_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        //required: true,
+      },
+      username: {
+        type: String,
+      },
+      profilePicture: {
+        type: String,
+      },
+    },
+  ],
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
