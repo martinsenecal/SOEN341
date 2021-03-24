f241d8e63505b825a68ef570da06f19331cced6f
KD <kim199@yahoo.com>
2021-03-23 21:01:03 -0400

    #82 added frontend validateFile jest test

---
+++ client/src/utils/validateFile.js
@@ -0,0 +1,16 @@
+  //validate file
+  const validateFile = (file) => {
+    const validTypes = [
+      'image/jpeg',
+      'image/jpg',
+      'image/png',
+      'image/gif',
+      'image/x-icon',
+    ];
+    if (validTypes.indexOf(file.type) === -1) {
+      return false;
+    }
+    return true;
+  };
+
+  module.exports = validateFile
