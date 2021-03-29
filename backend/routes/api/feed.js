ae0fa8b02cb13ce1d4d127bb825c4fee8b9f3d02
KD <kim199@yahoo.com>
2021-03-29 17:52:35 -0400

    #74 Added API for adding a like

--- backend/routes/api/feed.js
+++ backend/routes/api/feed.js
@@ -141,4 +141,35 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   }
 });
 
+
+router.post(
+  '/like/:id',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newLike = {
+        //Not a new collection in db
+        user: req.user.id,
+      };
+
+      post.likes.unshift(newLike);
+
+      await post.save();
+
+      res.json(post.likes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
