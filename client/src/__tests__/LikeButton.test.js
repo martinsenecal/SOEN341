a1ac33aa8feb0f28f1e4ec16843e3bd77e691a89
gkillick <gkillick@gmail.com>
2021-04-01 11:36:25 -0600

    #82 fixed test for like

--- client/src/__tests__/LikeButton.test.js
+++ client/src/__tests__/LikeButton.test.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
 import LikeButton from '../components/building-blocks/LikeButton';
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
+  document.body.removeChild(container);
   container = null;
 });
 
-it('renders with or without a name', () => {
+it('Verfiy heart selected icon is displayed for liked button', () => {
   act(() => {
-    render(<LikeButton liked="true" />, container);
+    ReactDOM.render(<LikeButton liked={true} />, container);
   });
-  
-  expect(container.classList.contains('fa-heart')).toBe(false);
+  const icon = container.querySelector('i');
+  const button = container.querySelector('button');
+  act(() => {
+    expect(icon.classList.contains('fa-heart')).toBe(true);
+  });
+
+  //test click event
+  act(() => {
+    button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+  //when like button is implemented should be false
+  expect(button.querySelector('i').classList.contains('fa-heart')).toBe(true);
+  //expect(document.title).toBe('You clicked 1 times');
+});
+
+it('Verfiy blank heart icon is displayed for not yet liked button', () => {
+  act(() => {
+    ReactDOM.render(<LikeButton liked={false} />, container);
+    const icon = container.querySelector('i');
+    expect(icon.classList.contains('fa-heart-o')).toBe(true);
+  });
 });
--- client/src/components/building-blocks/LikeButton.js
+++ client/src/components/building-blocks/LikeButton.js
@@ -1,28 +1,19 @@
 import React from 'react';
-const LikeButton = ({liked}) => {
 
-    if(liked)
-    {
-        return (
-            <button 
-                className = "like-button"
-                onClick = {() => console.log("clicked")}
-            >
-                <i className="fa fa-heart"></i>
-            </button>
-        )
-    }
-    else
-    {
-        return (
-            <button 
-                className = "like-button"
-                onClick = {() => console.log("clicked")}
-            >
-                <i className="fa fa-heart-o"></i>
-            </button>
-        )
-    }
-}
+const LikeButton = ({ liked }) => {
+  if (liked) {
+    return (
+      <button className="like-button" onClick={console.log('clicked')}>
+        <i className="fa fa-heart"></i>
+      </button>
+    );
+  } else {
+    return (
+      <button className="like-button" onClick={console.log('clicked')}>
+        <i className="fa fa-heart-o"></i>
+      </button>
+    );
+  }
+};
 
-export default LikeButton
+export default LikeButton;
